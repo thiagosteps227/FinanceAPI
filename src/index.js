4241b58747fc01dbe1e36459ed20ea4e670c3035
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-04-29 18:15:33 +0100

    api finished

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const { request } = require("express");
+const { request, response } = require("express");
 const express = require("express");
 const { v4: uuidv4 } = require("uuid");
 
@@ -98,5 +98,55 @@ app.post("/withdraw", verifyIfExistsAccountCPF, (request, response) => {
 
     return response.status(201).send();
 });
+
+app.get("/statement/date", verifyIfExistsAccountCPF, (request, response) => {
+
+    const { customer } = request;
+    const { date } = request.query;
+
+    const dateFormat = new Date(date + " 00:00");
+
+    const statement = customer.statement.filter(
+        (statement) => 
+        statement.created_at.toDateString() ===
+        new Date(dateFormat).toDateString());
+
+    return response.json(statement);
+});
+
+app.put("/account", verifyIfExistsAccountCPF, (request, response) => {
+
+    const { name } = request.body;
+    const { customer } = request;
+
+    customer.name = name;
+
+    return response.status(201).send();
+})
+
+app.get("/account", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } =  request;
+
+    return response.json(customer);
+})
+
+app.delete("/account", verifyIfExistsAccountCPF, (request, response) => {
+    
+    const { customer } = request;
+
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+
+})
+
+app.get("/balance", verifyIfExistsAccountCPF, (request, response) => {
+    const { customer } = request;
+
+    const balance = getBalance(customer.statement);
+
+    return response.json(balance);
+})
+
 
 app.listen(3333);
